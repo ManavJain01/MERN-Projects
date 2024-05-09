c3852106ea6548a197328bff04aed8ac6a240c9b
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-09 21:12:21 +0530

    commit

---
+++ server/index.js
@@ -0,0 +1,8 @@
+// Importing express and mongoDB Packages
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+app.listen(5000, ()=>{
+  console.log("Server is running on port 5000.");
+})
